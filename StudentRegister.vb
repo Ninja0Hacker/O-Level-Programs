f31c75dff57b0d785413499351e3605ecfb89b01
Sheran Sameer <103332864+Ninja0Hacker@users.noreply.github.com>
2023-02-02 15:01:43 +0400

    Create StudentRegister.vb
    Info About Students That you can store

---
+++ StudentRegister.vb
@@ -0,0 +1,24 @@
+Module Module1
+
+    Sub Main()
+        Dim Name As String
+        Dim marks As Integer
+        Dim avg As Single
+        Dim pass As Boolean
+        Dim gender As Char
+        Dim SchoolName As String
+        Dim Total As Integer
+
+        Name = "Sheran Sameer"
+        marks = 72
+        avg = 72.5
+        pass = True
+        gender = "Male"
+        SchoolName = "Roots Millenium College"
+        Total = 72
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
