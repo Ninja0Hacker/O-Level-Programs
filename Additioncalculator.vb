5c8b6441471249d2706c197594b3fe6893e36557
Sheran Sameer <103332864+Ninja0Hacker@users.noreply.github.com>
2023-02-02 14:55:22 +0400

    Create Additioncalculator.vb

---
+++ Additioncalculator.vb
@@ -0,0 +1,20 @@
+Module Module1
+
+    Sub Main()
+
+        Dim num1 As Integer
+        Dim num2 As Integer
+        Dim res As Integer
+
+        Console.WriteLine(" Enter first number : ")
+        num1 = Console.ReadLine()
+        Console.WriteLine(" Enter second number : ")
+        num2 = Console.ReadLine()
+        res = num1 + num2
+        Console.WriteLine(" The Result is " & res)
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
