9620646422ecde2c93a5314daab2e246aab59b5f
Sheran Sameer <103332864+Ninja0Hacker@users.noreply.github.com>
2023-02-02 14:53:27 +0400

    Create OddNumbers.vb

---
+++ OddNumbers.vb
@@ -0,0 +1,14 @@
+Module Module1
+
+    Sub Main()
+        For x = 100 To 199
+            x = x + 1
+            Console.WriteLine(x)
+
+        Next
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
